787f4525a8aae91b4f830eed010badf509149b38
NguyenMinhChien <minhchien03112003@gmail.com>
2024-10-18 16:53:08 +0700

    add zns zalo

--- src/routes/appointment.routes.js
+++ src/routes/appointment.routes.js
@@ -1,14 +1,27 @@
 const { Router } = require("express");
-const appointmentController = require('../controllers/appoinment/appointment.controller');
+const appointmentController = require("../controllers/appoinment/appointment.controller");
+const { sendZnsMessage } = require("../utils/znsService");
 const router = Router();
 
-router.post('/create', appointmentController.createAppointment);
-router.post('/update-status', appointmentController.updateStatusAppointment);
-router.get('/list-time', appointmentController.getAvailableTimeSlots);
-router.get('/', appointmentController.getAppointments);
-router.get('/:id', appointmentController.getAppointmentById);
-router.put('/:id', appointmentController.updateAppointment);
-router.delete('/:id', appointmentController.deleteAppointment);
-router.post('/query', appointmentController.getListAppointmentQuery)
+router.post("/create", appointmentController.createAppointment);
+router.post("/update-status", appointmentController.updateStatusAppointment);
+router.get("/list-time", appointmentController.getAvailableTimeSlots);
+router.get("/", appointmentController.getAppointments);
+router.get("/:id", appointmentController.getAppointmentById);
+router.put("/:id", appointmentController.updateAppointment);
+router.delete("/:id", appointmentController.deleteAppointment);
+router.post("/query", appointmentController.getListAppointmentQuery);
+router.post("/send-zns", async (req, res) => {
+  const { phone, templateData } = req.body;
+  //   console.log({ phone, templateData });
+
+  try {
+    const result = await sendZnsMessage(phone, templateData);
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error sending ZNS");
+  }
+});
 
 module.exports = router;
---
+++ src/utils/znsService.js
@@ -0,0 +1,48 @@
+const axios = require("axios");
+const crypto = require("crypto");
+
+// Config thông tin ứng dụng từ Zalo
+const APP_ID = "3743580504765137994";
+const APP_SECRET = "xCSojwQMxGjmX3Ri8755";
+const TEMPLATE_ID = "376137";
+const ZALO_API_URL = "https://business.openapi.zalo.me/message/template";
+
+// Tạo chữ ký cho API Zalo
+function createSignature(data) {
+  const hmac = crypto.createHmac("sha256", APP_SECRET);
+  hmac.update(JSON.stringify(data));
+  return hmac.digest("hex");
+}
+
+// Gửi tin nhắn ZNS
+async function sendZnsMessage(phone, data) {
+  console.log({ phone, data });
+  const requestData = {
+    phone, // Số điện thoại người nhận
+    template_id: TEMPLATE_ID, // Template ID
+    template_data: data, // Dữ liệu template (như tên khách hàng, thông tin sản phẩm,...)
+  };
+
+  const signature = createSignature(requestData);
+
+  try {
+    const response = await axios.post(ZALO_API_URL, requestData, {
+      headers: {
+        "Content-Type": "application/json",
+        access_token:
+          "nA3tKVF1G2d0fl9TzC95BCId_bNpsqnHeVgKMD2UO5EcXvD3Y8LKCBY2Wa3jl71iYAgXVCYKTrVuhv1cjxzwGlYbZsdMnaDT-DwGSCdzPLYywwjXpiiPIF_Uxb_CxnqjX8Nr2zsbGYMCfF0zlS8KETZMm2sgnN8BnU6eEfNoJ0ZIcPGEX9vHBUIlXbhLoNvZWU6qMk22V4_GaTDjvfCwPB6nsbpaWX1BWv6bQk-IVMcMXBrC_u49Gu-mys7jY1XLXEpYMixX9MwfzjDavCmvUzFSxa6grYTaslR0LglfRrlhWuDVZ-PNRyxranwLtNWCy_kE6fxQOohMyhD7XFfxODZTjHIKm5aNrDkpCQ6uV03ztvSkakbW3FVajpcRmtWhyF383wlq412HmF43yDmd3RZsyoHKR0DIWa3pt4Oc",
+        mac: signature,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error(
+      "Error sending ZNS:",
+      error.response ? error.response.data : error.message
+    );
+    throw error;
+  }
+}
+
+module.exports = { sendZnsMessage };
