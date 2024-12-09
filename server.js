1dc18ac265fadadaeb83960b68a5937f6244803d
nguyenminhchienit <104195481+nguyenminhchienit@users.noreply.github.com>
2024-12-09 23:18:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const customParseFormat = require("dayjs/plugin/customParseFormat");
 const dayjs = require("dayjs");
 dayjs.extend(customParseFormat);
 
-const app = express();
 app.use(fileUpload());
 app.use(
   cors({
@@ -20,29 +19,6 @@ app.use("/uploads", express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const authRoute = require("./src/routes/auth.routes");
-const fileRoute = require("./src/routes/file.routes");
-const userRoute = require("./src/routes/user.routes");
-const appointmentRoute = require("./src/routes/appointment.routes");
-const medicineRoute = require("./src/routes/medicine.routes");
-const medicalRecordRoute = require("./src/routes/medicalRecord.routes");
-const orderRoute = require("./src/routes/order.routes");
-const meetingRoute = require("./src/routes/meeting.routes");
-const statisticRoute = require("./src/routes/statistic.routes");
-const chatRoutes = require("./src/routes/chat.routes");
-const conversationRoutes = require("./src/routes/conversation.routes");
-
-app.use("/user", userRoute);
-app.use("/file", fileRoute);
-app.use("/auth", authRoute);
-app.use("/appointment", appointmentRoute);
-app.use("/medicine", medicineRoute);
-app.use("/medical-record", medicalRecordRoute);
-app.use("/order", orderRoute);
-app.use("/meeting", meetingRoute);
-app.use("/statistic", statisticRoute);
-app.use("/chat", chatRoutes);
-app.use("/conversation", conversationRoutes);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/src/view/serverRunning.html"));
